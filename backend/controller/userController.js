7c0b594ff3556add1334429399bee8cc4ab1c20e
dabeiStudent <20110515@student.hcmute.edu.vn>
2023-09-26 03:22:18 +0700

    user controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -5,7 +5,7 @@ const argon2 = require('argon2');
 const userRegister = async (req, res) => {
     const findUser = await User.findOne({ userName: req.body.userName });
     if (findUser) {
-        return res.status(400).json({ err: "Username already exists" });
+        return res.status(400).json({ err: "Username đã tồn tại" });
     } else {
         try {
             const hashedPassword = await argon2.hash(req.body.passWord);
@@ -18,12 +18,12 @@ const userRegister = async (req, res) => {
                 email: req.body.email,
                 passWord: hashedPassword,
                 phoneNumber: req.body.phoneNumber,
-                role: req.body.role,
+                role: "user",
                 image: req.body.image,
                 status: req.body.status
             })
-                .then(user => { return res.status(200).json({ msg: 'Created successfully' }) })
-                .catch(err => { return res.status(403).json({ err: "Email is already used" }) });
+                .then(user => { return res.status(200).json({ msg: 'Tạo tài khoản thành công' }) })
+                .catch(err => { return res.status(403).json({ err: "Email đã được sử dụng" }) });
         } catch (err) {
             return res.status(500).json({ err: err });
         }
