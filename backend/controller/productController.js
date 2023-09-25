f9c1cbbdf458d7ec7fd4eb64f7272f0925f9603a
dabeii <tranlan0310@gmail.com>
2023-09-25 20:19:43 +0700

    add new product & product types fix some error

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -9,32 +9,63 @@ const getAllProduct = (req, res) => {
 //get one
 //add new product & types of product
 const addNewProduct = (req, res) => {
-    Product.create({
-        name: req.body.name,
-        desc: req.body.desc,
-        pimage: req.body.pimage,
-        tags: req.body.tags,
-        release: req.body.release,
-        made: req.body.made,
-        category: req.body.category,
-        brand: req.body.brand,
-        inStock: req.body.inStock,
-        ptype: {
-            productName: req.body.name,
-            SKU: req.body.SKU,
-            typeName: req.body.typeName,
-            importPrice: req.body.importPrice,
-            sellPrice: req.body.sellPrice,
-            quantity: req.body.quantity,
-            sold: req.body.sold,
-            image: req.body.image,
-            voucher: req.body.voucher,
-            test: req.body.test
-        }
-    })
-        .then(
-            Product_type.create({
+    try {
+        Product.create({
+            name: req.body.name,
+            desc: req.body.desc,
+            pimage: req.body.pimage,
+            tags: req.body.tags,
+            release: req.body.release,
+            made: req.body.made,
+            category: req.body.category,
+            brand: req.body.brand,
+            inStock: req.body.inStock,
+            ptype: {
                 productName: req.body.name,
+                SKU: req.body.SKU,
+                typeName: req.body.typeName,
+                importPrice: req.body.importPrice,
+                sellPrice: req.body.sellPrice,
+                quantity: req.body.quantity,
+                sold: req.body.sold,
+                image: req.body.image,
+                voucher: req.body.voucher,
+                test: req.body.test
+            }
+        })
+            .then(
+                Product_type.create({
+                    productName: req.body.name,
+                    SKU: req.body.SKU,
+                    typeName: req.body.typeName,
+                    importPrice: req.body.importPrice,
+                    sellPrice: req.body.sellPrice,
+                    quantity: req.body.quantity,
+                    sold: req.body.sold,
+                    image: req.body.image,
+                    voucher: req.body.voucher
+                })
+                    .then(product => {
+                        return res.status(200).json({ msg: 'Added successfully' })
+                    })
+                    .catch(err => { return res.status(401).json({ err: err }) })
+            )
+            .catch(err => { return res.status(401).json({ err: err }) })
+    }
+    catch (err) {
+        return res.status(401).json({ err: err })
+    }
+};
+
+const addNewProductType = async (req, res) => {
+    try {
+        const findSKU = await Product_type.findOne({ SKU: req.body.SKU });
+        if (findSKU) {
+            return res.status(401).json({ err: "SKU already exist" })
+        }
+        else {
+            Product_type.create({
+                productName: req.body.productName,
                 SKU: req.body.SKU,
                 typeName: req.body.typeName,
                 importPrice: req.body.importPrice,
@@ -44,60 +75,38 @@ const addNewProduct = (req, res) => {
                 image: req.body.image,
                 voucher: req.body.voucher
             })
-                .then(product => {
-                    return res.status(200).json({ msg: 'Added successfully' })
-                })
-                .catch(err => { return res.status(401).json({ err: err }) })
-        )
-        .catch(err => { return res.status(401).json({ err: err }) })
-};
-
-const addNewProductType = async (req, res) => {
-    const findSKU = await Product_type.findOne({ SKU: req.body.SKU });
-    if (findSKU) {
-        return res.status(401).json({ err: "SKU already exist or Product not found" })
-    }
-    else {
-        Product_type.create({
-            productName: req.body.productName,
-            SKU: req.body.SKU,
-            typeName: req.body.typeName,
-            importPrice: req.body.importPrice,
-            sellPrice: req.body.sellPrice,
-            quantity: req.body.quantity,
-            sold: req.body.sold,
-            image: req.body.image,
-            voucher: req.body.voucher
-        })
-            .then(
-                Product.findById(req.body.productId)
-                    .then((product) => {
-                        product.ptype.push({
-                            productName: req.body.productName,
-                            SKU: req.body.SKU,
-                            typeName: req.body.typeName,
-                            importPrice: req.body.importPrice,
-                            sellPrice: req.body.sellPrice,
-                            quantity: req.body.quantity,
-                            sold: req.body.sold,
-                            image: req.body.image,
-                            voucher: req.body.voucher
+                .then(
+                    Product.findById(req.body.productId)
+                        .then((product) => {
+                            product.ptype.push({
+                                productName: req.body.productName,
+                                SKU: req.body.SKU,
+                                typeName: req.body.typeName,
+                                importPrice: req.body.importPrice,
+                                sellPrice: req.body.sellPrice,
+                                quantity: req.body.quantity,
+                                sold: req.body.sold,
+                                image: req.body.image,
+                                voucher: req.body.voucher
+                            })
+                            product.save();
                         })
-                        product.save();
-                    })
-                    .then(result => {
-                        return res.status(200).json({ msg: "Added successfully!" });
-                    })
-                    .catch(error => {
-                        return res.status(401).json({ err: error })
-                    })
-            )
-            .catch(err => {
-                return res.status(401).json(err);
-            })
+                        .then(result => {
+                            return res.status(200).json({ msg: "Added successfully!" });
+                        })
+                        .catch(error => {
+                            return res.status(401).json({ err: error })
+                        })
+                )
+                .catch(err => {
+                    return res.status(401).json(err);
+                })
 
+        }
+    } catch (err) {
+        return res.status(400).json({ err: err });
     }
-}
+};
 //test git remote
 
 module.exports = { getAllProduct, addNewProduct, addNewProductType };
