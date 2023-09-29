b7dcc93ed84809b05815b0fca825c6ae951dd4ee
NgVinhThien <nguyennvinhthien@gmail.com>
2023-09-29 21:24:02 +0700

    delete & update voucher

--- backend/controller/voucherController.js
+++ backend/controller/voucherController.js
@@ -15,6 +15,23 @@ const getAllVoucher= async (req, res)=> {
     res.status(200).json(vouchers);
 }
 
+const getVoucherById= async (req, res)=> {
+    const voucherId= req.params.vid;
+
+    let voucher;
+    try {
+        voucher= await Voucher.findById(voucherId);
+    }catch (err){
+        return res.status(404).json({err: "Đã có lỗi xảy ra. Vui lòng thử lại sau."});
+    }
+
+    if(!voucher){
+        return res.status(404).json({err: "Không tìm thấy voucher."});
+    }
+
+    res.status(200).json(voucher);
+}
+
 const createVoucher= async (req, res)=> {
     const {name, desc, percent, startDate, endDate}= req.body;
     
@@ -37,7 +54,7 @@ const createVoucher= async (req, res)=> {
 
 const addProductToVoucher= async (req, res)=>{
     const voucherId= req.params.vid;
-    const productId= req.body;
+    const {productId}= req.body;
 
     let existingVoucher;
     try{
@@ -50,33 +67,86 @@ const addProductToVoucher= async (req, res)=>{
         return res.status(404).json({err: "Không tìm thấy voucher."});
     }
 
-    
+    let productInVoucher;
+    productInVoucher= existingVoucher.productList.find(product=> product.equals(productId));
 
-    // let existingProduct;
-    // try{
-    //     existingProduct= existingVoucher.productList.find(productId);
-    // }catch(error){
-    //     return res.status(404).json({err: error});
-    // }
+    if(productInVoucher){
+        return res.status(404).json({err: "Sản phẩm đã được thêm vào trước đó."});
+    }
 
+    let existingProduct;
+    try{ 
+        existingProduct= await Product.findById(productId);
+    } catch(error){
+        return res.status(404).json({err: error})
+    }
 
-    // if(!existingProduct){
-    //     return res.status(404).json({err: "Không tìm thấy sản phẩm."});
-    // }
+    if(!existingProduct){
+        return res.status(404).json({err: "Không tìm thấy sản phẩm."});
+    }
 
-    // try{
-    //     const sess= await mongoose.startSession();
-    //     sess.startTransaction();
-    //     await existingVoucher.productList.push(existingProduct);
-    //     await existingVoucher.save();
-    //     await existingProduct.vouchers.push(existingVoucher);
-    //     await existingProduct.save();
-    //     await sess.commitTransaction();
-    // }catch(error){
-    //     return res.status(404).json({err: "Không thể thêm. Vui lòng thử lại sau."})
-    // }
-    return res.status(200).json(existingVoucher.productList);
+    try{
+        const sess= await mongoose.startSession();
+        sess.startTransaction();
+        await existingVoucher.productList.push(existingProduct);
+        await existingVoucher.save();
+        await existingProduct.vouchers.push(existingVoucher);
+        await existingProduct.save();
+        await sess.commitTransaction();
+    }catch(error){
+        return res.status(404).json({err: "Không thể thêm. Vui lòng thử lại sau."})
+    }
+    return res.status(200).json({msg: "Thêm thành công."});
 }
+
+const removeProductFromVoucher= async (req, res)=>{
+    const voucherId= req.params.vid;
+    const {productId}= req.body;
+
+    let existingVoucher;
+    try{
+        existingVoucher= await Voucher.findById(voucherId).populate('productList');
+    }catch(error){
+        return res.status(404).json({err: error});
+    }
+
+    if(!existingVoucher){
+        return res.status(404).json({err: "Không tìm thấy voucher."});
+    }
+
+    let productInVoucher;
+    productInVoucher= existingVoucher.productList.find(product=> product.equals(productId));
+
+    if(!productInVoucher){
+        return res.status(404).json({err: "Sản phẩm không thuộc khuyến mãi này."});
+    }
+
+    let existingProduct;
+    try{ 
+        existingProduct= await Product.findById(productId);
+    } catch(error){
+        return res.status(404).json({err: error})
+    }
+
+    if(!existingProduct){
+        return res.status(404).json({err: "Không tìm thấy sản phẩm."});
+    }
+
+    try{
+        const sess= await mongoose.startSession();
+        sess.startTransaction();
+        await existingVoucher.productList.pop(existingProduct);
+        await existingVoucher.save();
+        await existingProduct.vouchers.pop(existingVoucher);
+        await existingProduct.save();
+        await sess.commitTransaction();
+    }catch(error){
+        return res.status(404).json({err: "Không thể xóa. Vui lòng thử lại sau."})
+    }
+    return res.status(200).json({msg: "Đã xóa xong."});
+}
+
+
 
 const getProductByVoucherId= async (req, res)=> {
     const voucherId= req.params.vid;
@@ -91,4 +161,73 @@ const getProductByVoucherId= async (req, res)=> {
     res.status(200).json(voucherWithProduct.productList);
 }
 
-module.exports = { createVoucher, addProductToVoucher, getAllVoucher, getProductByVoucherId };
+const updateVoucher= async (req, res)=> {
+    const voucherId= req.params.vid;
+    const { name, desc, percent, startDate, endDate }= req.body;
+
+    let existingVoucher;
+    try {
+        existingVoucher= await Voucher.findById(voucherId);
+    }catch (err){
+        return res.status(404).json({err: err});
+    }
+
+    if(!existingVoucher){
+        return res.status(404).json({err: "Không tìm thấy voucher. Vui lòng thử lại sau."});
+    }
+
+    existingVoucher.name= name;
+    existingVoucher.desc= desc;
+    existingVoucher.percent= percent;
+    existingVoucher.start= startDate;
+    existingVoucher.end= endDate;
+
+    try{
+        await existingVoucher.save();
+    }catch(err){
+        return res.status(404).json({err: err})
+    }
+
+    res.status(200).json({msg: "Cập nhật thành công."});
+
+}
+
+const deleteVoucher= async (req, res)=>{
+    const voucherId= req.params.vid;
+
+    let voucher;
+    try{
+        voucher= await Voucher.findById(voucherId).populate('productList');
+    }catch (err){
+        return res.status(404).json({err: "Không thể xóa. Vui lòng thử lại sau."});
+    }
+
+    if(!voucher){
+        return res.status(400).json({err: "Không tìm thấy voucher. Vui lòng thử lại sau."});
+    };
+
+    try {
+        const sess= await mongoose.startSession();
+        sess.startTransaction();
+        voucher.productList.map(async (product)=> {
+            product.vouchers.pop(voucher)
+            await product.save();
+        })
+        await voucher.deleteOne();
+        await sess.commitTransaction();
+    }catch(err){
+        return res.status(404).json({err: "Không thể xóa. Đã có lỗi xảy ra."})
+    }
+
+    res.status(200).json({msg: "Đã xóa."})
+}
+
+module.exports = { 
+    createVoucher,
+    addProductToVoucher, 
+    getAllVoucher, 
+    getVoucherById,
+    getProductByVoucherId,
+    removeProductFromVoucher,
+    updateVoucher,
+    deleteVoucher };
--- backend/routes/voucherRoutes.js
+++ backend/routes/voucherRoutes.js
@@ -5,8 +5,12 @@ const emailSending = require('../utils/emailUtils');
 
 router.get('/get-all-voucher', voucherController.getAllVoucher);
 router.get('/get-product/:vid', voucherController.getProductByVoucherId);
+router.get('/get-voucher/:vid', voucherController.getVoucherById);
 router.post('/add-new-voucher', voucherController.createVoucher);
 router.put('/add-product-to-voucher/:vid', voucherController.addProductToVoucher);
+router.put('/remove-product-from-voucher/:vid', voucherController.removeProductFromVoucher);
+router.put('/update-voucher/:vid', voucherController.updateVoucher);
+router.delete('/delete-voucher/:vid', voucherController.deleteVoucher);
 
 //notice chosen voucher to all user 
 router.post('/notice-voucher-to-all', emailSending.sendVoucherMail);
