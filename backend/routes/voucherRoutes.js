689ed3f815653acb1e4b2a50c57aa6c472f11154
NgVinhThien <nguyennvinhthien@gmail.com>
2023-09-29 21:32:04 +0700

    delete & update voucher function

--- backend/routes/voucherRoutes.js
+++ backend/routes/voucherRoutes.js
@@ -9,14 +9,13 @@ router.use(cookieParser());
 router.get('/get-all-voucher', voucherController.getAllVoucher);
 router.get('/get-product/:vid', voucherController.getProductByVoucherId);
 router.get('/get-voucher/:vid', voucherController.getVoucherById);
-router.post('/add-new-voucher', voucherController.createVoucher);
-router.put('/add-product-to-voucher/:vid', voucherController.addProductToVoucher);
-router.put('/remove-product-from-voucher/:vid', voucherController.removeProductFromVoucher);
-router.put('/update-voucher/:vid', voucherController.updateVoucher);
-router.delete('/delete-voucher/:vid', voucherController.deleteVoucher);
-
-router.post('/', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.createVoucher);
-router.put('/addProduct', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.addProductToVoucher);
+// router.post('/add-new-voucher', voucherController.createVoucher);
+router.put('/add-product-to-voucher/:vid', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.addProductToVoucher);
+router.put('/remove-product-from-voucher/:vid', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.removeProductFromVoucher);
+router.put('/update-voucher/:vid', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.updateVoucher);
+router.delete('/delete-voucher/:vid', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.deleteVoucher);
+router.post('/add-new-voucher', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.createVoucher);
+// router.put('/addProduct', tokenCheck.checkJWT, tokenCheck.isAdmin, voucherController.addProductToVoucher);
 
 //notice chosen voucher to all user 
 router.post('/notice-voucher-to-all', tokenCheck.checkJWT, tokenCheck.isAdmin, emailSending.sendVoucherMail);
