6fae74a377e7af71f0dc86a426b989285c570bb3
dabeii <tranlan0310@gmail.com>
2023-10-01 16:09:26 +0700

    sửa vài lỗi

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,12 +1,14 @@
-const express= require('express');
-const router= express.Router();
+const express = require('express');
+const router = express.Router();
+const tokenCheck = require('../middlewares/tokenCheck');
+const cookieParser = require('cookie-parser');
+router.use(cookieParser());
+const categoryController = require('../controller/categoryController');
 
-const categoryController= require('../controller/categoryController');
-
-router.post('/add-new-category', categoryController.addCategory);
+router.post('/add-new-category', tokenCheck.checkJWT, tokenCheck.isAdmin, categoryController.addCategory);
 router.get('/get-sub-category/:cid', categoryController.getSubCategory);
 router.get('/get-main-category', categoryController.getMainCategory);
-router.put('/update-category/:cid', categoryController.updateCategory);
-router.delete('/delete-category/:cid', categoryController.deleteCategory);
+router.put('/update-category/:cid', tokenCheck.checkJWT, tokenCheck.isAdmin, categoryController.updateCategory);
+router.delete('/delete-category/:cid', tokenCheck.checkJWT, tokenCheck.isAdmin, categoryController.deleteCategory);
 
-module.exports= router;
+module.exports = router;
