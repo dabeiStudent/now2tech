8dd4ff34240976c9e037ad3a6efd8d822aecb177
dabeii <tranlan0310@gmail.com>
2023-10-01 21:42:28 +0700

    cải tiến thuật toán xủ lí vấn đề reset-password

--- backend/utils/emailUtils.js
+++ backend/utils/emailUtils.js
@@ -37,9 +37,12 @@ const sendEmailReset = async (req, res) => {
         if (!findUser) {
             return res.status(404).json({ err: "Không tìm thấy email trong hệ thống" });
         }
+        //secret là key dùng để tạo one time link
+        //ở hàm tiếp theo sẽ kiểm tra bằng cách verify lại token với secret
+        const secret = process.env.JWT_KEY + findUser.passWord
         const resetToken = jwt.sign({
             email: req.body.email,
-        }, process.env.JWT_KEY, { expiresIn: 300 }); //seconds
+        }, secret, { expiresIn: 300 }); //seconds
         RspToken.create({
             rspToken: resetToken,
             createAt: time,
@@ -50,7 +53,7 @@ const sendEmailReset = async (req, res) => {
             to: req.body.email,
             subject: 'Xác thực yêu cầu thiết lập lại mật khẩu',
             html: `<h2 style="color:black;">Bạn đã có yêu cầu thiết lập lại mật khẩu, hãy nhấn vào đường link bên dưới:</h2>
-            <p style="color:red;">http://${localhost}/user/reset-password/check-token?rspToken=${resetToken}&time=${time}</p>
+            <p style="color:red;">http://${localhost}/user/reset-password/check-token?rspToken=${resetToken}&time=${time}&email=${req.body.email}</p>
             <p style="color:black;">Để xác thực email này là của bạn</p>
             <p style="color:black;">(Liên kết sẽ bị vô hiệu hóa sau 5 phút)</p>`
         };
@@ -61,7 +64,7 @@ const sendEmailReset = async (req, res) => {
                 // console.log(`Nội dung url reset: `);
                 // console.log(resetToken);
                 // console.log(time);
-                console.log(`Email xác thực: http://${localhost}/user/reset-password/check-token?rspToken=${resetToken}&time=${time}`);
+                console.log(`Email xác thực: http://${localhost}/user/reset-password/check-token?rspToken=${resetToken}&time=${time}&email=${req.body.email}`);
                 return res.status(200).json({ msg: "Hãy kiểm tra email" });
             }
         });
@@ -71,7 +74,7 @@ const sendEmailReset = async (req, res) => {
 }
 const resetPassword = async (req, res) => {
     try {
-        const { rspToken, time } = req.query;
+        const { rspToken, time, email } = req.query;
         const findRsToken = await RspToken.findOne({ rspToken: rspToken, createAt: time })
         const now = Date.now();
         const calculateValidTime = now - findRsToken.createAt;
@@ -81,12 +84,15 @@ const resetPassword = async (req, res) => {
             return res.json({ err: "Đã hơn 5 phút" });
         }
         if (findRsToken) {
-            const verify = jwt.verify(findRsToken.rspToken, process.env.JWT_KEY);
-            const email = verify.email
             const findUser = await User.findOne({ email });
             if (!findUser) {
                 return res.status(404).json({ err: "Không tìm thấy email trong hệ thống" });
             }
+            //secret là key dùng để tạo one time link
+            const secret = process.env.JWT_KEY + findUser.passWord;
+            //kiểm tra xem JWT_KEY+password còn giống với kết quả khi thực hiện hàm send không 
+            //nếu hàm bung lỗi => passWord đã được xử lý đổi lại trước đó nên 2 key để sign và verify rspToken khác nhau
+            jwt.verify(findRsToken.rspToken, secret);
             const newPassword = await generateNewPassword();
             const hashPassword = await argon2.hash(newPassword);
             findUser.passWord = hashPassword;
@@ -108,14 +114,15 @@ const resetPassword = async (req, res) => {
                     console.log(calculateValidTime)
                     console.log(`Email gửi thành công, mật khẩu mới là: ${newPassword}`);
                     // console.log(`Mật khẩu hash: ${hashPassword}`)
-                    return res.status(200).json({ msg: "Đổi mật khẩu thành công" });
+                    return res.status(200).json({ msg: "Đổi mật khẩu thành công, hãy kiểm tra email để nhận mật khẩu mới" });
                 }
             });
         } else {
             return res.json({ err: "Không hợp lệ" });
         }
     } catch (err) {
-        return res.status(500).json({ err: "Có lỗi xảy ra" });
+        console.log({ err: err });
+        return res.status(500).json({ err: err });
     }
 }
 
