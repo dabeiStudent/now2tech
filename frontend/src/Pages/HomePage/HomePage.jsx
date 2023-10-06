605563032e9882f5222beb77a71ee70e74fdd49f
NgVinhThien <nguyennvinhthien@gmail.com>
2023-10-06 08:54:48 +0700

    update homepage

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -21,20 +21,29 @@ const HomePage = () => {
 
     return (
         <div>
-            <BigBanner />
-            <Slider />
-            <div className="all-product-container">
-                <p className="all-product-container__title">TẤT CẢ SẢN PHẨM</p>
-                <div className="all-product-container__prod-list">
-                    {products.map(product => (
-                        <ProductCard
-                            id={product._id}
-                            name={product.name}
-                            price={product.sellPrice} />
-                    ))}
+            <BigBanner/>
+            <div className="home-page-container">
+                <Slider/>
+                <div className="voucher-list-container">
+                    <div className="voucher-list-banner">
+                        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/10/banner/Deal-ngon-tgdd1-1200x120-1.png" alt="voucher-list-banner" />
+                    </div>
+                    <VoucherList/>
+                </div>
+                <div className="all-product-container">
+                    <p className="all-product-container__title">TẤT CẢ SẢN PHẨM</p>
+                    <div className="all-product-container__prod-list">
+                        {products.map(product=> (
+                            <ProductCard
+                                key={product._id}
+                                id={product._id}
+                                name= {product.name}
+                                price= {product.sellPrice}/>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </div> 
     )
 }
 
