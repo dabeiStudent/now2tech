395bd5d4d01875fb20a57bf7c6f4163506889d3a
dabeii <tranlan0310@gmail.com>
2023-10-27 21:52:03 +0700

    Xây dựng trang quản lý đơn hàng

--- frontend/src/Pages/Cart/CartPage.jsx
+++ frontend/src/Pages/Cart/CartPage.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
@@ -13,42 +14,42 @@ import getCookie from '../../ultis/getCookie';
 import { OrderContext } from '../../ultis/orderContext';
 
 const CartPage = () => {
-  const cart= useContext(CartContext);
-  const orderContext= useContext(OrderContext);
+  const cart = useContext(CartContext);
+  const orderContext = useContext(OrderContext);
 
   const usernameEncoded = getCookie('username');
   const username = decodeURIComponent(usernameEncoded);
-  const navigate= useNavigate();
-  const [selectedItems, setSelectedItems]= useState([]);
-  const [selectAll, setSelectAll]= useState(false);
+  const navigate = useNavigate();
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
 
-  const selectAllHandler= ()=> {
-    if(selectAll){
+  const selectAllHandler = () => {
+    if (selectAll) {
       setSelectedItems([]);
-    } else{
+    } else {
       setSelectedItems(cart.items)
     }
     setSelectAll(!selectAll);
   }
 
-  const checkboxChangeHandler= (e)=>{
-    const itemInCart= cart.items.find(item=> item.id === e.target.value);
+  const checkboxChangeHandler = (e) => {
+    const itemInCart = cart.items.find(item => item.id === e.target.value);
     setSelectAll(false)
-    if(selectedItems.includes(itemInCart)){
-      setSelectedItems(selectedItems.filter(item=> item.id !== itemInCart.id))
+    if (selectedItems.includes(itemInCart)) {
+      setSelectedItems(selectedItems.filter(item => item.id !== itemInCart.id))
     } else {
       setSelectedItems([...selectedItems, itemInCart])
     }
   }
 
-  useEffect(()=>{
-    if(cart.items.length === selectedItems.length){
+  useEffect(() => {
+    if (cart.items.length === selectedItems.length) {
       setSelectAll(true);
     }
   }, [selectedItems, cart.items.length]);
 
-  const placeOrderHandler= async ()=> {
-    if(username !== 'false'){
+  const placeOrderHandler = async () => {
+    if (username !== 'false') {
       orderContext.setSelectedItems(selectedItems);
       navigate('/thong-tin-giao-hang')
     } else {
@@ -56,40 +57,40 @@ const CartPage = () => {
       navigate('/login')
     }
   }
-  
+
   return (
     <div className='cart-page'>
-        <div className='cart-page__main'>
-          <p className='cart-page__title'>Giỏ hàng của bạn:</p>
-          {cart.items.length === 0 ? (
-            <div className='cart-empty'>
-              <FontAwesomeIcon className='cart-plus__icon' icon={faCartPlus}/>
-              <p>Giỏ hàng của bạn đang trống</p>
-              <a href="/">Quay về trang chủ</a>
+      <div className='cart-page__main'>
+        <p className='cart-page__title'>Giỏ hàng của bạn:</p>
+        {cart.items.length === 0 ? (
+          <div className='cart-empty'>
+            <FontAwesomeIcon className='cart-plus__icon' icon={faCartPlus} />
+            <p>Giỏ hàng của bạn đang trống</p>
+            <NavLink to="/">Quay về trang chủ</NavLink>
+          </div>
+        ) : (
+          <div>
+            <div className='cart-page__list-item'>
+              <Form className='list-item__form'>
+                <Form.Check className='custom-checkbox' label='Chọn tất cả' onChange={selectAllHandler} checked={selectAll} value={'select-all'} name="item" inline />
+                {cart.items.length > 0 && cart.items.map(item => (
+                  <div className='list-item__single-item' key={item.id}>
+                    <Form.Check checked={selectedItems.includes(item)} onChange={checkboxChangeHandler} className='custom__check-box' value={item.id} name='item' aria-label='option' />
+                    <Item id={item.id} name={item.name} price={item.price} qty={item.qty} vouchers={item.vouchers} />
+                  </div>
+                ))}
+              </Form>
             </div>
-          ) : (
-            <div>
-              <div className='cart-page__list-item'>
-                <Form className='list-item__form'>
-                  <Form.Check className='custom-checkbox' label='Chọn tất cả' onChange={selectAllHandler} checked={selectAll} value={'select-all'} name="item" inline />
-                  { cart.items.length > 0 && cart.items.map(item=> (
-                    <div className='list-item__single-item' key={item.id}>
-                      <Form.Check checked={selectedItems.includes(item)} onChange={checkboxChangeHandler} className='custom__check-box' value={item.id} name='item' aria-label='option'/>
-                      <Item id={item.id} name={item.name} price={item.price} qty={item.qty} vouchers={item.vouchers}/>
-                    </div>
-                  ))}
-                </Form>   
-              </div>
-              <div className='total-cost'>
-                  <span>Tạm tính ({selectedItems.reduce((acc, current)=> acc + current.qty, 0)} sản phẩm):</span>
-                  <span>{formatPrice(selectedItems.reduce((acc, current)=> acc + (current.vouchers ? ((current.price * (100 - current.vouchers.percent) / 100) * current.qty): (current.price * current.qty)), 0))}</span>
-              </div>
-              <div className='cart-page__btn'>
-                <Button onClick={placeOrderHandler} className='cart-page__custom-btn' variant='danger' disabled={selectedItems.length === 0}>ĐẶT HÀNG</Button>
-              </div>
+            <div className='total-cost'>
+              <span>Tạm tính ({selectedItems.reduce((acc, current) => acc + current.qty, 0)} sản phẩm):</span>
+              <span>{formatPrice(selectedItems.reduce((acc, current) => acc + (current.vouchers ? ((current.price * (100 - current.vouchers.percent) / 100) * current.qty) : (current.price * current.qty)), 0))}</span>
             </div>
-          )}
-        </div>
+            <div className='cart-page__btn'>
+              <Button onClick={placeOrderHandler} className='cart-page__custom-btn' variant='danger' disabled={selectedItems.length === 0}>ĐẶT HÀNG</Button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
