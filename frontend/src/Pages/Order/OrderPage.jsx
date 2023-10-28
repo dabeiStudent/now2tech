2f99fc3558fb89dd81c311963dc020ac7568ba08
dabeii <tranlan0310@gmail.com>
2023-10-28 21:16:13 +0700

    Xây dựng trang quản lý đơn hàng

--- frontend/src/Pages/Order/OrderPage.jsx
+++ frontend/src/Pages/Order/OrderPage.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,93 +10,93 @@ import OrderItem from './components/OrderItem';
 import { formatPrice } from '../../ultis/formatPrice';
 
 const OrderPage = () => {
-  let {oid}= useParams();
-  const [order, setOrder]= useState();
+  let { oid } = useParams();
+  const [order, setOrder] = useState();
 
-  useEffect(()=> {
-      axios.get(`http://localhost:5000/order/get-order/${oid}`)
-      .then(res=> setOrder(res.data))
-      .catch(err=> console.log(err));
+  useEffect(() => {
+    axios.get(`http://localhost:5000/order/get-order/${oid}`)
+      .then(res => setOrder(res.data))
+      .catch(err => console.log(err));
   }, [oid]);
 
   return (
     <div>
       {order ? (
-         <div className='order-page'>
-         <div className='order-page__top' >
-           {/* <button><FontAwesomeIcon icon={faArrowLeft}/></button> */}
-           <h2>Chi tiết đơn hàng: #{order._id}</h2>
-         </div>
-         
-         <div className='address-container'>
-           <p className='order-page__title'>Thông tin nhận hàng:</p>
-           <Row>
-             <Col lg={3}><span>Người nhận:</span></Col>
-             <Col lg={9}><span>{order.user.lastName} {order.user.firstName}</span></Col>
-           </Row>   
-           <Row>
-             <Col lg={3}><span>Điện thoại:</span></Col>
-             <Col lg={9}><span>{order.user.phoneNumber}</span></Col>
-           </Row>   
-           <Row>
-             <Col lg={3}><span>Email: </span></Col>
-             <Col lg={9}><span>{order.user.email}</span></Col>
-           </Row> 
-           <Row>
-             <Col lg={3}><span>Địa chỉ: </span></Col>
-             <Col lg={9}><span>{order.address}</span></Col>
-           </Row>
-   
-         </div>
-         <div className='order-container'>
-           <p className='order-page__title'>Thông tin đơn hàng:</p>
-           <Row>
-             <Col lg={3}><span>Trạng thái:</span></Col>
-             {order.status === 'Not_proccessed' ? (<Col lg={9}><span>Chờ xác nhận</span></Col>) 
-             : (order.status === 'Processing' ? (<Col lg={9}><span>Đang xử lý</span></Col>)
-             : (order.status === 'Shipped' ? (<Col lg={9}><span>Đang giao</span></Col>) 
-             : (order.status === 'Delivered' ? (<Col lg={9}><span>Đã giao</span></Col>)
-             : (<Col lg={9}><span>Đã hủy</span></Col>))))}
-           </Row>   
-           <Row>
-             <Col lg={3}><span>Phương thức thanh toán:</span></Col>
-             <Col lg={9}><span>{order.paymentMethod}</span></Col>
-           </Row> 
-           <Row>
-             <Col lg={3}><span>Trạng thái thanh toán:</span></Col>
-             {order.paymentStatus.isPaid === true ? 
-             (<Col lg={9}><span>Đã thanh toán {order.paymentStatus.paidAt}</span></Col>)
-             : (<Col lg={9}><span>Chưa thanh toán</span></Col>)}
-             
-           </Row>   
-         </div>
-         <div className='detail-container'>
-           <p className='order-page__title'>Thông tin sản phẩm:</p>
-           <ul className='list-order-item'>
-            {order.items.map(item=> (
-              <OrderItem 
-                key={item._id} 
-                name={item.name}
-                qty={item.qty}
-                image={item.image}
-                price={item.price}/>))
-            }
-           </ul>
-           <div className='detail-container__fees'>
-             <Row>
-               <Col lg={4}><span>Tạm tính ({order.items.reduce((acc, current)=> acc + current.qty, 0)} sản phẩm): </span></Col>
-               <Col className='custom-col'lg={8}><span>{formatPrice(order.price)}</span></Col>
-             </Row>
-             <Row>
-               <Col lg={4}><span>Phí giao hàng: </span></Col>
-               <Col className='custom-col'lg={8}><span>{formatPrice(order.price)}</span></Col>
-             </Row>
-             <Row>
-               <Col lg={4}><span>Tổng cộng: </span></Col>
-               <Col className='custom-col'lg={8}><span>{formatPrice(order.totalPrice)}</span></Col>
-             </Row>
-           </div>
-           {/* {isPending ? (
+        <div className='order-page'>
+          <div className='order-page__top' >
+            {/* <button><FontAwesomeIcon icon={faArrowLeft}/></button> */}
+            <h2>Chi tiết đơn hàng: #{order._id}</h2>
+          </div>
+
+          <div className='address-container'>
+            <p className='order-page__title'>Thông tin nhận hàng:</p>
+            <Row>
+              <Col lg={3}><span>Người nhận:</span></Col>
+              <Col lg={9}><span>{order.user.lastName} {order.user.firstName}</span></Col>
+            </Row>
+            <Row>
+              <Col lg={3}><span>Điện thoại:</span></Col>
+              <Col lg={9}><span>{order.user.phoneNumber}</span></Col>
+            </Row>
+            <Row>
+              <Col lg={3}><span>Email: </span></Col>
+              <Col lg={9}><span>{order.user.email}</span></Col>
+            </Row>
+            <Row>
+              <Col lg={3}><span>Địa chỉ: </span></Col>
+              <Col lg={9}><span>{order.address}</span></Col>
+            </Row>
+
+          </div>
+          <div className='order-container'>
+            <p className='order-page__title'>Thông tin đơn hàng:</p>
+            <Row>
+              <Col lg={3}><span>Trạng thái:</span></Col>
+              {order.status === 'Not_proccessed' ? (<Col lg={9}><span>Chờ xác nhận</span></Col>)
+                : (order.status === 'Processing' ? (<Col lg={9}><span>Đang xử lý</span></Col>)
+                  : (order.status === 'Shipped' ? (<Col lg={9}><span>Đang giao</span></Col>)
+                    : (order.status === 'Delivered' ? (<Col lg={9}><span>Đã giao</span></Col>)
+                      : (<Col lg={9}><span>Đã hủy</span></Col>))))}
+            </Row>
+            <Row>
+              <Col lg={3}><span>Phương thức thanh toán:</span></Col>
+              <Col lg={9}><span>{order.paymentMethod}</span></Col>
+            </Row>
+            <Row>
+              <Col lg={3}><span>Trạng thái thanh toán:</span></Col>
+              {order.paymentStatus.isPaid === true ?
+                (<Col lg={9}><span>Đã thanh toán {order.paymentStatus.paidAt}</span></Col>)
+                : (<Col lg={9}><span>Chưa thanh toán</span></Col>)}
+
+            </Row>
+          </div>
+          <div className='detail-container'>
+            <p className='order-page__title'>Thông tin sản phẩm:</p>
+            <ul className='list-order-item'>
+              {order.items.map(item => (
+                <OrderItem
+                  id={item._id}
+                  name={item.name}
+                  qty={item.qty}
+                  image={item.image}
+                  price={item.price} />))
+              }
+            </ul>
+            <div className='detail-container__fees'>
+              <Row>
+                <Col lg={4}><span>Tạm tính ({order.items.reduce((acc, current) => acc + current.qty, 0)} sản phẩm): </span></Col>
+                <Col className='custom-col' lg={8}><span>{formatPrice(order.price)}</span></Col>
+              </Row>
+              <Row>
+                <Col lg={4}><span>Phí giao hàng: </span></Col>
+                <Col className='custom-col' lg={8}><span>{formatPrice(order.price)}</span></Col>
+              </Row>
+              <Row>
+                <Col lg={4}><span>Tổng cộng: </span></Col>
+                <Col className='custom-col' lg={8}><span>{formatPrice(order.totalPrice)}</span></Col>
+              </Row>
+            </div>
+            {/* {isPending ? (
              <div>Loading...
              </div>
            ): (
@@ -104,20 +104,20 @@ const OrderPage = () => {
                <PayPalButtons onApprove={onApprove} createOrder={createOrder} onError={onError}/>
              </div>
            )} */}
-         </div>
-         {/* <div className='payment-container'>
+          </div>
+          {/* <div className='payment-container'>
            
          </div> */}
-       </div>
+        </div>
       ) : (
         <div>Loading...</div>
       )}
 
     </div>
-    
-    
-   
-    
+
+
+
+
   )
 }
 
