933fa6f9db0991548088632c45aec3c8765ee7aa
NgVinhThien <nguyennvinhthien@gmail.com>
2023-10-29 16:26:20 +0700

    Chỉnh sửa  navlink

--- frontend/src/components/UIElement/NavLinks.jsx
+++ frontend/src/components/UIElement/NavLinks.jsx
@@ -1,5 +1,6 @@
 import React, {useContext} from "react";
 import { NavLink } from 'react-router-dom';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faCartShopping, faPhone, faUser, faArrowRightToBracket, faBarsProgress } from '@fortawesome/free-solid-svg-icons'
 
@@ -50,32 +51,44 @@ const NavLinks = () => {
                     <span>Giỏ hàng</span>
                 </NavLink>
             </li>
-            {username !== 'false'
-                ? <li>
-                    <NavLink to='/my-profile' className="nav-links__login">
-                        <FontAwesomeIcon className="nav-links__icon-login" icon={faUser} />
-                        <span>Hello, {userLoggedIn.userName} ({userLoggedIn.role})</span>
-                    </NavLink>
+            {username !== 'false' ? (
+                <li>
+                    <div className="dropdown-element">
+                        <button className="dropdown-element__btn"><FontAwesomeIcon className="nav-links__icon-login" icon={faUser} />Hello, {userLoggedIn.userName} ({userLoggedIn.role})</button>
+                        <div className="dropdown-element__menu">
+                            <NavLink to='/my-profile'>Tài khoản của tôi</NavLink>
+                            
+                            {username === "admin" && (
+                                <NavLink to="/now2tech-management" className="nav-links__login">
+                                Quản lý
+                                {/* <FontAwesomeIcon className="nav-links__icon-login" icon={faBarsProgress} /> */}
+                                {/* <span>Quản lý</span> */}
+                                </NavLink>
+                            )}
+                        </div>
+                    </div>
                 </li>
-                : <li>
+            ) : (
+                <li>
                     <NavLink to="/login" className="nav-links__login">
                         <FontAwesomeIcon className="nav-links__icon-login" icon={faArrowRightToBracket} />
                         <span>Đăng nhập</span>
                     </NavLink>
-                </li>}
-            {username === "admin" &&
-                <li>
-                    <NavLink to="/now2tech-management" className="nav-links__login">
-                        <FontAwesomeIcon className="nav-links__icon-login" icon={faBarsProgress} />
-                        <span>Quản lý</span>
-                    </NavLink>
-                </li>}
+                </li>
+            )}
+                {/* //  <li>
+                //     <NavLink to='/my-profile' className="nav-links__login">
+                //         <FontAwesomeIcon className="nav-links__icon-login" icon={faUser} />
+                //         <span>Hello, {userLoggedIn.userName} ({userLoggedIn.role})</span>
+                //     </NavLink>
+                // </li> */}
             {/* <li>
                 <NavLink to="/history">
                     <FontAwesomeIcon className="nav-links__icon-user" icon={faUser} />
                     <span>Tài khoản và đơn hàng</span>
                 </NavLink>
             </li> */}
+            
         </ul>
     )
 }
