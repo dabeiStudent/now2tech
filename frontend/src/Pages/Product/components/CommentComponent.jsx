0300c554113647bd9cd55c8e0c84a82074dd9d9c
NgVinhThien <nguyennvinhthien@gmail.com>
2023-11-02 20:53:44 +0700

    fix reply comment function

--- frontend/src/Pages/Product/components/CommentComponent.jsx
+++ frontend/src/Pages/Product/components/CommentComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 import './CommentComponent.css';
 import CommentList from './CommentList';
